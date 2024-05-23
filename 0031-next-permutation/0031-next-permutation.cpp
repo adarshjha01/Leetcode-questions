211fcaac198a6972cf553331cb941e12503bbe64
Adarsh jha <117096531+adarshjha01@users.noreply.github.com>
2024-05-23 15:55:51 +0530

    Time: 4 ms (57.52%), Space: 14.5 MB (86.97%) - LeetHub

--- 0031-next-permutation/0031-next-permutation.cpp
+++ 0031-next-permutation/0031-next-permutation.cpp
@@ -3,9 +3,9 @@ public:
     void nextPermutation(vector<int>& nums) {
         int index=-1;
         int n=nums.size();
-        // finding breaking point
+        // finding breaking point from backwards
         for ( int i=n-2;i>=0;i--){
-            if(nums[i]<nums[i+1]) {
+            if(nums[i]<nums[i+1]) { //finding break point
                 index=i;
                 break;
             }
@@ -15,8 +15,8 @@ public:
             return (reverse(nums.begin(), nums.end()));
         }
         // if there exists some breaking point then swap it with the just greater than index element
-        for ( int i=n-1;i>=0;i--){
-            if(nums[i]>nums[index]){ 
+        for ( int i=n-1;i>=0;i--){ // iteration from backwards
+            if(nums[i]>nums[index]){ // the moment element is greater than nums [i] swap it
                 swap(nums[i], nums[index]);
                 break;
             };
