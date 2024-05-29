6585e38cb548ea27c0dfe571816d4e66211d9e81
Adarsh jha <117096531+adarshjha01@users.noreply.github.com>
2024-05-29 10:18:57 +0530

    Time: 637 ms (5.01%), Space: 246.8 MB (5.07%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.cpp
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.cpp
@@ -1,19 +1,18 @@
 class Solution {
 public:
     int lengthOfLongestSubstring(string s) {
-        int maxlen=0; 
-        int n=s.length();
-        for (int l =0;l<n;l++){
+        int maxlen=0; // to hold the maximum length of the string
+        int n=s.length(); // string length
+        for (int l =0;l<n;l++){ // iteration over the loop 
             int r=l;
             unordered_map<char,int> mp;
-            while(r<n){
-                if(mp.find(s[r])!=mp.end()) break;
-                mp[s[r]]++;
-                r++;
+            while(r<n){ // inner loop
+                if(mp.find(s[r])!=mp.end()) break; // if the element is in the map then break form inner loop and calulate the length 
+                mp[s[r]]++; // if element is not there push it into the map
+                r++; // increase the pointer 
             }
-            maxlen=max(maxlen,r-l);
+            maxlen=max(maxlen,r-l); // update the length of maxlen is element is there in the map
         }
-        return maxlen;
-
+        return maxlen; // returns answer
     }
 };
