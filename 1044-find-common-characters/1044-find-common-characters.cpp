7ba6d3ea1a009e943040158dd0653e5afe8b4737
Adarsh jha <117096531+adarshjha01@users.noreply.github.com>
2024-06-05 14:48:24 +0530

    Time: 13 ms (28.73%), Space: 13.3 MB (26.88%) - LeetHub

---
+++ 1044-find-common-characters/1044-find-common-characters.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    vector<string> commonChars(vector<string>& words) {
+        unordered_map<char, int> mp1;
+        unordered_map<char, int> mp2;
+        int n=words.size();
+        for (int i =0,j=0;j<words[i].length();j++){ // mp1 will be holding the first string words[i];
+            mp1[words[i][j]]++;
+        }
+        for (int i =1;i<n;i++){
+            for ( int j=0;j<words[i].length();j++){
+                if(mp1.find(words[i][j])!=mp1.end()){
+                    mp2[words[i][j]]++;
+                    mp1[words[i][j]]--;
+                    if(mp1[words[i][j]]==0) mp1.erase(words[i][j]);
+                }
+            }
+            mp1.clear();
+            mp1.insert(mp2.begin(),mp2.end());
+            mp2.clear();
+        }   
+        vector<string>  ans;
+      for (auto it : mp1) {
+            for (int i = 0; i < it.second; ++i) {
+                ans.push_back(std::string(1, it.first)); // Convert char to string before pushing
+            }
+        }
+        return ans;
+
+    }
+};
