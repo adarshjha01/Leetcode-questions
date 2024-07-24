95e34316f8cb739bf29829a7e4dd5bb1474d44c8
Adarsh jha <117096531+adarshjha01@users.noreply.github.com>
2024-07-24 11:48:13 +0530

    Time: 6 ms (21.01%), Space: 9.7 MB (33.11%) - LeetHub

--- 0090-subsets-ii/0090-subsets-ii.cpp
+++ 0090-subsets-ii/0090-subsets-ii.cpp
@@ -1,30 +1,28 @@
-#include <vector>
-#include <algorithm>
-
 class Solution {
 public:
-    void func(int ind, std::vector<int> &temp, std::vector<int> &nums, int n, std::vector<std::vector<int>> &ans) {
-        ans.push_back(temp);
 
-        for (int i = ind; i < n; ++i) {
-            // Skip duplicates
-            if (i > ind && nums[i] == nums[i - 1]) continue;
-            temp.push_back(nums[i]);
-            func(i + 1, temp, nums, n, ans);
-            temp.pop_back();
+    void func(int ind, vector<int> &temp, vector<int> &nums, int n, set<vector<int>> &st){
+        if(ind==n){
+            st.insert(temp);
+            return;
         }
+        temp.push_back(nums[ind]);
+        func(ind+1, temp, nums, n, st);
+        temp.pop_back();
+        func(ind+1, temp, nums, n, st);
     }
+    vector<vector<int>> subsetsWithDup(vector<int>& nums) {
+        vector<vector<int>> ans;
+        set<vector<int>> st;
+        vector<int> temp;
+        int n=nums.size();
+        sort(nums.begin(), nums.end());
+        func(0,temp,nums,n,st);
 
-    std::vector<std::vector<int>> subsetsWithDup(std::vector<int>& nums) {
-        std::vector<std::vector<int>> ans;
-        std::vector<int> temp;
-        int n = nums.size();
-
-        // Sort the input array to handle duplicates
-        std::sort(nums.begin(), nums.end());
-
-        func(0, temp, nums, n, ans);
-
+        for(auto it: st){
+            ans.push_back(it);
+        }
         return ans;
+
     }
 };
