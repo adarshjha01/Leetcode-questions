7b5f43698966c4fbfb6594e979d078e9c3f591c2
Adarsh jha <117096531+adarshjha01@users.noreply.github.com>
2024-10-14 15:45:12 +0530

    Time: 190 ms (69.83%), Space: 76 MB (79.55%) - LeetHub

--- 2616-maximal-score-after-applying-k-operations/2616-maximal-score-after-applying-k-operations.cpp
+++ 2616-maximal-score-after-applying-k-operations/2616-maximal-score-after-applying-k-operations.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
     long long maxKelements(vector<int>& nums, int k) {
-        priority_queue<int> pq(nums.begin(), nums.end()); // max-heap
+        priority_queue<int> pq(nums.begin(), nums.end()); 
         long long ans = 0;
         
         while(k--){
@@ -9,7 +9,6 @@ public:
             pq.pop();
             ans += maxElem;
 
-            // Modify the element as per the problem's condition
             if(maxElem % 3 != 0){
                 pq.push(maxElem / 3 + 1);
             } else {
